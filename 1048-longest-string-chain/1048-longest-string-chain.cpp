bd91600bf9df4b9f53580c7d832f70c7dfe6c73e
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-15 19:38:45 +0530

    Time: 109 ms (72.38%), Space: 17.3 MB (48.86%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& words) {
-        int k = 0;
-        sort(words.begin(),words.end(),sortByLength);
-        unordered_map<string,int>dp;
-        for(auto word:words){
-            for(int i=0;i<word.size();i++){
-                string pred = word.substr(0,i)+word.substr(i+1);
-                dp[word] = max(dp[word],dp[pred]+1);
+    int longestStrChain(vector<string> &words) {
+        unordered_map<string, int> dp;
+        int res = 1;
+        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() < r.size(); });
+        for (string word : words) {
+            dp[word] = 1;
+            for (int i = 0; i < word.size(); i++) {
+                string prev = word.substr(0, i) + word.substr(i + 1);
+                if (dp.find(prev) != dp.end()) {
+                    dp[word] = dp[prev] + 1;
+                    res = max(res, dp[word]);
+                }
             }
-            k = max(k,dp[word]);
         }
-        return k;
-    }
-    static bool sortByLength(string& s1,string& s2){
-        return s1.length()<s2.length();
+        return res;
     }
 };
