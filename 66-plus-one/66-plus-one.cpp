d2e0c70e9b491f0c417fce88195f7f69ed5258ac
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-16 22:55:30 +0530

    Time: 3 ms (62.66%), Space: 8.8 MB (12.42%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     vector<int>plusOne(vector<int>& digits) {
-        for (int i=digits.size(); i--; digits[i] = 0)
-            if (digits[i]++ < 9)
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; --i){
+            if (digits[i] == 9){
+                digits[i] = 0;
+            }else{
+                digits[i]++;
                 return digits;
-        digits[0]++;
+            }
+        }
+        digits[0] =1;
         digits.push_back(0);
         return digits;
     }
