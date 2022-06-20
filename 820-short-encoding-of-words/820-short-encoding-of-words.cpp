9d43cbc1d9055f6f6ee766c1c3913005be67a765
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-20 18:38:31 +0530

    Time: 58 ms (80.20%), Space: 17.4 MB (73.27%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
         unordered_set<string>s1(words.begin(),words.end());
-        for(string &word:words){
+        for(string word:s1){
+            if (s1.find(word) != s1.end())
                 for(int i=1;i<word.size();i++){
                     s1.erase(word.substr(i));
                 }
         }
         int out = s1.size();
-        for(string a:s1){
+        for(auto a:s1){
             out+= a.size();
         }
         return out;
