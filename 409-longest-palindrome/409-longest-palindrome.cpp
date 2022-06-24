7eadcdd56e9d93b13907fc056ac75176cbec735a
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-24 23:07:09 +0530

    Time: 8 ms (23.58%), Space: 6.7 MB (49.11%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int out = 0;
-        int odd = 0;
         unordered_map<char,int>ump;
         for(int i=0;i<s.size();i++){
             ump[s[i]]++;
         }
-        if(ump.size()==1)return ump[s[0]];
-        int point;
+        int odd = 0;
         for(auto x:ump){
-            point = x.second;
-            if(point%2==0){
-                out += point; 
-            }else{
-                out +=point-1;
-                odd++;
-            }
+            if(x.second%2!=0) odd++;
         }
-        if(odd>=1)return out+1;
-        
-        return out;
+        int val = 0;
+        if(odd>=1)val =1;
+        return s.size() - odd +val; 
     }
 };
