943e4b09c77a00404e82393c806b1b953b268dbc
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-26 11:41:02 +0530

    Time: 67 ms (25.00%), Space: 27.5 MB (63.69%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -5,14 +5,10 @@ public:
         int high = nums.size()-1;
         int mid;
         while(low<=high){
-            mid = (low+high)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]<target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
+            mid = low + (high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) low = mid+1;    
+            else high = mid-1;
         }
         return -1;
     }
