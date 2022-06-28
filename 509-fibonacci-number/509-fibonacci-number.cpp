ecfeecc31cc5fd89a8c6bde9bc944788fbb99660
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-28 22:28:30 +0530

    Time: 4 ms (52.93%), Space: 5.9 MB (41.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        else
-            return fib(n-1)+fib(n-2); 
+        if(n<2)return n;
+        int a = 0,b=1,c=0;
+        for(int i=0;i<n-1;i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 };
