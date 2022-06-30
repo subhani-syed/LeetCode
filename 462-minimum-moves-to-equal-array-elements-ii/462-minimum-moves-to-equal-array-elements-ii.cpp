a8064d5117324cd78cf77485cd891001202e9e2c
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-06-30 10:01:29 +0530

    Time: 19 ms (35.76%), Space: 11 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,11 +3,9 @@ public:
     int minMoves2(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        int mid = nums[n/2];
-        int out = 0;
-        for(int i=0;i<nums.size();i++){
-            out += abs(nums[i]-mid);
-        }
-        return out;
+        int left = 0, right = n- 1, moves = 0;
+        while (left < right)
+            moves += nums [right--] - nums [left++];
+        return moves;
     }
 };
