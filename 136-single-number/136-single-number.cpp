9a4f59ba0e05593d1ff7f2b1e7801b8877e0bd1e
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-07-07 23:30:18 +0530

    Time: 24 ms (76.97%), Space: 17 MB (51.22%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int out = 0;
-        for(int i=0;i<nums.size();i++){
+        int out = 0,n=nums.size();
+        for(int i=0;i<n;i++){
             out = out ^ nums[i];
         }
         return out;
