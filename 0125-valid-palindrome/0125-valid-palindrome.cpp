4c631cf6dfb9796eedfae6ccfe3728c20b6ff8b1
Subhani Syed <64395664+subhani-syed@users.noreply.github.com>
2022-10-21 19:24:40 +0530

    Time: 16 ms (30.32%), Space: 7.3 MB (78.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newstr="";
-        for(int i=0;i<s.length();i++){
-            if((s[i]>=48&&s[i]<=57) || (s[i]>=65 && s[i]<=90)||(s[i]>=97&&s[i]<=122)){
-                if(s[i]>=65&&s[i]<=90){
-                    newstr+= s[i]+32;
-                }else{
-                    newstr+=s[i];
-                }
-            }
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--){    
+            while (isalnum(s[i]) == false && i < j)
+                i++; 
+            while (isalnum(s[j]) == false && i < j)
+                j--; 
+            if(toupper(s[i]) != toupper(s[j]))
+                return false; 
         }
-        for(int j=0;j<newstr.length()/2;j++){
-            if(newstr[j]!=newstr[newstr.length()-1-j]){
-                return false;
-            }
-        }
-        return true;
+    
+    return true;
     }
 };
